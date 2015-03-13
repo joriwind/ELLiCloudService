90b414b49ec80dc1e17716743217dd46023cb81b
joriwind <jori.winderickx@student.uhasselt.be>
2015-03-13 13:00:34 +0200

    adjusted

--- databaseHandler.js
+++ databaseHandler.js
@@ -4,35 +4,8 @@ var Node = require('./objects/Node');
 //connect to our database
 var conString = "postgres://postgres:winderickx@localhost/ELLiDatabase"
 //PostGres Connection String
-/*
-pg.connect(conString, function(err, client, done) {
 
-  if (err) {
-    return console.error('error fetching client from pool', err);
-  }
-  client.query('SELECT * FROM nodes', function(err, result) {
-    done();
-    if (err) {
-      return console.error('error running query', err);
-    }
-    console.log(result.rows[0]);
-    
-    client.end();
-  });
 
-});*/
-
-//pg.connect(conString, databaseHandler)
-/*
-var databaseHandler = function(err, client, done){
-   if (err) {
-    return console.error('error fetching client from pool', err);
-  }
-  this.client = client;
-   
-}
-  */
-  
 var databaseHandler = function(){
    //pg.connect(conString, this.init);
    this.client = new pg.Client(conString);
@@ -108,14 +81,6 @@ databaseHandler.prototype.closeConnection = function(){
       console.log("Client does not exist");
    }
 }
-/*
-databaseHandler.prototype.testHandler = function(err, client, done) {
-   handler.init(err, client,done);
-   //console.log(handler.client);
-   handler.getNodes();
-   //handler.closeConnection();
-}*/
 
-//var handler = new databaseHandler();
 
 module.exports = databaseHandler;
