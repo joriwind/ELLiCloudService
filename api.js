03b77b8921ec9f702dfac38a3c49c0aefb22fedb
joriwind <jori.winderickx@student.uhasselt.be>
2015-03-17 14:32:51 +0200

    Added https

--- api.js
+++ api.js
@@ -12,34 +12,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use('/api', routes);
-app.set('port', process.env.PORT || 8000);
- //This is our route middleware
-
 //app.set('port', process.env.PORT || 8000);
+ //This is our route middleware
  
 /*var server = app.listen(app.get('port'), function() {
    
   console.log('Express server listening on port ' + server.address().port);
 });*/
 
-https.createServer(options, app).listen(443);
 
-//module.exports = app;
+var privateKey  = fs.readFileSync('certificates/akey-key.pem');
+var certificate = fs.readFileSync('certificates/akey-cert.pem');
 
-/*
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+var credentials = {key: privateKey, cert: certificate};
 
-app.get('/test', function (req, res) {
-  res.send('Huh?')
-})
+var httpsServer = https.createServer(credentials, app);
 
-var server = app.listen(3000, function () {
-
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log('Ell-i Express cloud service listening at http://%s:%s', host, port)
-
-})*/
+httpsServer.listen(443)
