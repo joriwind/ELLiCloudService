4e7ed80bc031318faccbcfde968b127468101e66
joriwind <jori.winderickx@student.uhasselt.be>
2015-03-24 13:28:20 +0200

    added some log

--- routes/elliroutes.js
+++ routes/elliroutes.js
@@ -23,6 +23,7 @@ router.route('/nodes/:id').get(function(req, res) {
 router.route('/nodes/').post(function(req, res) {
    console.log("POST: " + "param: " + JSON.stringify(req.params)+" body: "  + JSON.stringify(req.body));
    var var1 = req.body.node;
+   console.log("POST additional the node: " + req.body.node);
    dbHandler.setNode(new Node(JSON.parse(var1)), function(obj){
       res.send(JSON.stringify(obj));
    });
